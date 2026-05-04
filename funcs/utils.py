4535ce005cf1c1795300539f13528d5d8f53a7be
albebelen <albebelen@gmail.com>
2026-05-04 23:49:59 +0200

    add retreival function for semantic chunking

--- funcs/utils.py
+++ funcs/utils.py
@@ -19,8 +19,20 @@ def read_document(path):
 4. retrieve the top k  chunks 
 returns the best chunks
 # '''
+
+# use for other chunking except subdoc and pageindex
 def retrieve_top_k(chunks, query, embeddings, k=5):
     chunk_embeds = embeddings.embed_documents(chunks)
+    query_embed = embeddings.embed_query(query)
+
+    sims = cosine_similarity([query_embed], chunk_embeds)[0]
+    top_k_idx = np.argsort(sims)[-k:][::-1]
+
+    return [chunks[i] for i in top_k_idx]    
+
+def retrieve_top_k_alt(chunks, query, embeddings, k=5):
+    chunks_to_embed = [c["chunk_content"] for c in chunks]
+    chunk_embeds = embeddings.embed_documents(chunks_to_embed)
     query_embed = embeddings.embed_query(query)
 
     sims = cosine_similarity([query_embed], chunk_embeds)[0]
